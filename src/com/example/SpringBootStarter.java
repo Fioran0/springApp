0f8e024d9fb8cf44306ea8e218a27322b4af24dc
Evgeniy <Fiorano96@yandex.ru>
2020-02-14 10:50:43 +0300

    purchase finish

--- src/com/example/SpringBootStarter.java
+++ src/com/example/SpringBootStarter.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 import javax.transaction.Transactional;
@@ -20,7 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @Transactional
 public class SpringBootStarter implements CommandLineRunner {
 
